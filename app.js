c7dcc895a5c491f1caa25cdd7c8ee2e22da2fd07
SieGra <kjetil.lorentzen@gmail.com>
2018-09-25 00:54:56 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+
+
+let express = require('express'); //require express
+let app = express();    //setting variable app to express-call
+let bodyParser = require('body-parser');
+
+/* Body parser */
+app.use(bodyParser.json()); //Body parser now handles json
+app.use(bodyParser.urlencoded({extended: false})); 
+
+app.use(express.static(__dirname + '/public')); //setting express to serve statics. __dirname refeers to the folder the file is locaated. statics are in the public folder.
+
+/*  Pug */
+app.set('view engine', 'pug');          //Setting the view engine to pug.
+app.set('views', __dirname + '/views'); // Setting the views root folder.
+
+/* Routes */
+let routes = require('./routes/index');
+app.use('/', routes);
+
+
+/* Error handling */
+app.use((req, res, next) => {
+    let err = new Error('Page not found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next)=>{
+    let sessionInfo = {
+        message: err.message,
+        code: err.status,
+        error: {},
+        title: 'Error'
+    }
+
+    res.status(err.status || 500);
+    res.render('error', sessionInfo);
+
+});
+
+/* Setting the server to listen at port xxxx */
+app.listen(3000, ()=> {
+    console.log('App listening on port 3000')
+});
