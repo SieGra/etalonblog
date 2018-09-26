d4d11a94191148c411b69b9a49d10776fd11e598
SieGra <kjetil.lorentzen@gmail.com>
2018-09-26 14:19:29 +0200

    layout comming along

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,12 @@
 const mainHeader = document.getElementById('header');
-
+const figImgs = document.querySelectorAll('.quoteImg');
+const body = document.getElementById('body');
 document.addEventListener('scroll', ()=>{
     mainHeader.style.backgroundPositionY = `${window.scrollY/50 + 50}%`;
+    body.style.backgroundPositionY = `${window.scrollY/100 + 50}%`;
+
+    figImgs.forEach((element, i)=> {
+        figImgs[i].style.top = `${(window.scrollY  / 60) - 40}%`;
+    });
+
 });
